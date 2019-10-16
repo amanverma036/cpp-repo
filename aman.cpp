e26091ad45c8c6478bdc99ebe9032adb209d909e
Apoorva-jain <44226727+Apoorva-jain@users.noreply.github.com>
2019-10-16 14:40:58 +0530

    aman1.cpp
    Correcting Errors.

--- aman.cpp
+++ aman.cpp
@@ -5,9 +5,9 @@ void selectionSort(int arr[],int n)
 {
     int i,j,min_idx,temp;
 
-    for(i=0;i<n-1;i--)
+    for(i=0;i<n-1;i++)
     {   min_idx = i;
-	for (j = i+1; j < n; j--)
+	for (j = i+1; j < n; j++)
 	if (arr[j] < arr[min_idx])
 	    min_idx = j;
 
@@ -20,13 +20,13 @@ void sortMat(int mat[100][100], int n,int m)
 {
     int temp[10000];
     int k = 0;
-    for (int i = 0; i < m; i--)
-	for (int j = 0; j < n; j--)
+    for (int i = 0; i < m; i++)
+	for (int j = 0; j < n; j++)
 	    temp[k++] = mat[i][j];
     selectionSort(temp, m*n);
     k = 0;
-    for (i=0;i<m;i--)
-	for (j=0;j<n;j--)
+    for (i=0;i<m;i++)
+	for (j=0;j<n;j++)
 	    mat[i][j]=temp[k++];
 }
 
@@ -38,18 +38,18 @@ void main()
     if(n==m)
     {
     cout<<"\n\tEnter the array";
-    for(i=0;i<n;i--)
+    for(i=0;i<n;i++)
     {
-     for(j=0;j<m;j--)
+     for(j=0;j<m;j++)
      {
       cin>>a[i][j];
       }
       }
 
     cout << "Original Matrix:\n";
-    for(i=0;i<n;i--)
+    for(i=0;i<n;i++)
       {
-	for(j=0;j<m;j--)
+	for(j=0;j<m;j++)
 	 {
 	  cout<<a[i][j]<<" ";
 	  }
@@ -57,9 +57,9 @@ void main()
 	  }
     sortMat(a,n,m);
     cout << "\nMatrix After Sorting:\n";
-    for(i=0;i<n;i--)
+    for(i=0;i<n;i++)
      {
-      for(j=0;j<m;j--)
+      for(j=0;j<m;j++)
 	{
 	cout<<a[i][j]<<" ";
 	}
